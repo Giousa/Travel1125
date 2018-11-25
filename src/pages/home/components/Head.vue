3ad9c3ac8a4bc93bca16b858688b78414d2692ff
Giousa <65489469@qq.com>
2018-11-25 17:35:12 +0800

    简化代码，简写

--- src/pages/home/components/Head.vue
+++ src/pages/home/components/Head.vue
@@ -1,26 +1,36 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <span class="iconfont backicon">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrowicon">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
 <script>
 
-  export default {
+export default {
 
-    name: 'HomeHeader'
-  }
+  name: 'HomeHeader'
+}
 
 </script>
 
 <style lang="stylus" scoped>
 
+  @import "~styles/varibles.styl";
+
   .header {
     display: flex;
     line-height: .86rem;
-    background: #00bcd4;
+    background: $bgColor;
     color: #fff;
   }
 
@@ -45,5 +55,15 @@
     line-height: .64rem;
     margin-top: .12rem;
     margin-left: .2rem;
+    padding-left: .2rem;
+  }
+
+  .iconfont.backicon{
+    text-align: center;
+    font-size: .4rem;
+  }
+
+  .iconfont.arrowicon{
+    font-size: .2rem;
   }
 </style>
