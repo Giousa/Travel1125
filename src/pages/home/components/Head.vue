0e3cf9f611781b6baa7234fd5111cb485730d756
Giousa <65489469@qq.com>
2018-11-25 15:43:55 +0800

    头部布局

--- src/pages/home/components/Head.vue
+++ src/pages/home/components/Head.vue
@@ -1,16 +1,49 @@
 <template>
-  <div>这个是头部文件</div>
+  <div class="header">
+    <div class="header-left">返回</div>
+    <div class="header-input">输入城市/景点/游玩主题</div>
+    <div class="header-right">城市</div>
+  </div>
 </template>
 
 <script>
 
-export default {
+  export default {
 
-  name: 'HomeHeader'
-}
+    name: 'HomeHeader'
+  }
 
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
 
+  .header {
+    display: flex;
+    line-height: .86rem;
+    background: #00bcd4;
+    color: #fff;
+  }
+
+  .header-left {
+    width: .64rem;
+    float: left;
+  }
+
+  .header-right {
+    width: 1.24rem;
+    float: right;
+    text-align: center;
+  }
+
+  .header-input {
+    text-align: left;
+    flex: 1;
+    background: #ffffff;
+    border-radius: .1rem;
+    color: #ccc;
+    height: .64rem;
+    line-height: .64rem;
+    margin-top: .12rem;
+    margin-left: .2rem;
+  }
 </style>
