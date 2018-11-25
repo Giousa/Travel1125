fb45c38dd77abde108107e646b9352ca6a6146f5
Giousa <65489469@qq.com>
2018-11-25 13:47:05 +0800

    修改index文件配置

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <div>
+
+    <div class="home">Home首页</div>
+
+    <router-link to="/list">列表页</router-link>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+
+</script>
+
+<style scoped>
+
+  .home {
+    font-size: 50px;
+  }
+
+</style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <div>
+    <div>List列表</div>
+
+    <router-link to="/">返回首页</router-link>
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'List'
+}
+
+</script>
+
+<style scoped>
+
+</style>
